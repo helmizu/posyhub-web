31665bb132947dd62b6fe70d1f7947d553e03478
Helmi Zulfikar <helmi.zulfikar20@gmail.com>
2023-09-12 23:01:16 +0700

    update component column

--- src/components/Column.tsx
+++ src/components/Column.tsx
@@ -1,12 +1,17 @@
 import { Typography, theme } from 'antd';
 import React from 'react';
 
-const Column = () => {
+interface ColumnProps {
+  text?: string;
+  subtext?: string;
+}
+
+const Column: React.FC<ColumnProps> = ({ text, subtext }) => {
   const { token: { colorTextSecondary } } = theme.useToken();
   return (
     <div style={{ display: 'flex', flexDirection: 'column', gap: 4 }}>
-      <Typography.Text style={{ fontWeight: 500 }}>text</Typography.Text>
-      <Typography.Text style={{ color: colorTextSecondary }}>text</Typography.Text>
+      {!!text && (<Typography.Text style={{ fontWeight: 500 }}>{text}</Typography.Text>)}
+      {!!subtext && (<Typography.Text style={{ color: colorTextSecondary }}>{subtext}</Typography.Text>)}
     </div>
   );
 };
