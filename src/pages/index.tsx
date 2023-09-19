30c6799cd08c0d85f43e4a96e84b7c024e4ce5e1
Helmi Zulfikar Suprayitno <helmi.zulfikar20@gmail.com>
2023-09-19 22:07:04 +0700

    fix home routing

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,11 +2,17 @@ import React, { useEffect } from 'react';
 import Layout from '@/components/templates/Layout';
 import HomeContainer from '@/containers/Home';
 import { useRouter } from 'next/router';
+import { useSession } from 'next-auth/react';
 
 const Home = () => {
   const router = useRouter();
+  const { status } = useSession();
   useEffect(() => {
-    router.replace('/dashboard');
+    if (status === 'authenticated') {
+      router.replace('/dashboard');
+    } else if (status === 'unauthenticated') {
+      router.replace('/login');
+    }
   }, []);
 
   return (
