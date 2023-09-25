d4b7db5f65e063008772335e6395ecab1f0af42d
Helmi Zulfikar <helmi.zulfikar20@gmail.com>
2023-09-25 18:44:00 +0700

    fix: update index redirecting

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,13 +7,14 @@ const Home = () => {
   const { token: { colorPrimary } } = theme.useToken();
   const router = useRouter();
   const { status } = useSession();
+
   useEffect(() => {
     if (status === 'authenticated') {
       router.replace('/dashboard');
     } else if (status === 'unauthenticated') {
       router.replace('/login');
     }
-  }, []);
+  }, [status]);
 
   return (
     <div style={{ height: '100vh', width: '100%', display: 'flex', alignItems: 'center', justifyContent: 'center', flexDirection: 'column', gap: 16 }}>
